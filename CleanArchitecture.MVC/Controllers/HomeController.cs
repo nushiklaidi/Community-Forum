6f8729c9e0819b6e4cfb3b40e2d0201f54acbe02
Nikolaos Nousi <nikolaos.nousi@iteam.gr>
2020-11-27 17:17:05 +0200

    Add custome exception

--- CleanArchitecture.MVC/Controllers/HomeController.cs
+++ CleanArchitecture.MVC/Controllers/HomeController.cs
@@ -1,23 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using CleanArchitecture.MVC.Models;
-using Microsoft.AspNetCore.Authorization;
-using CleanArchitecture.Application.Model;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace CleanArchitecture.MVC.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+    {      
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -30,10 +19,13 @@ namespace CleanArchitecture.MVC.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+
+            return View("Error");
         }
     }
 }
