9e56a6b52be2bfe642c71acfe0d407848adc2068
Klaidi Nushi <nushiklaidi@gmail.com>
2020-11-27 09:16:02 +0200

    Add View Model

---
+++ CleanArchitecture.Application/ViewModels/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int Rating { get; set; }
+        public string ProfileImageUrl { get; set; }
+        public DateTime MemberSince { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<SelectListItem> RoleList { get; set; }
+    }
+}
